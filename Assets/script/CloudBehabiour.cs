28bc8b1330668f8cceb77863dc31714987165f10
Nitae@SDSharp <kcastellanos278@unab.edu.co>
2023-02-04 22:41:32 -0500

    agregadas nubes

--- Assets/script/CloudBehabiour.cs
+++ Assets/script/CloudBehabiour.cs
@@ -4,22 +4,36 @@ using UnityEngine;
 
 public class CloudBehabiour : MonoBehaviour
 {
-    public float velocity = 1;
+    public float velocity = 0.1f;
     public bool _isRight = true;
+
+    public List<Sprite> sprites = new List<Sprite>();
 
 
     private void OnEnable()
     {
+        int index = Random.Range(0,sprites.Count);
+        int layer = Random.Range(-3,2);
+
+        GetComponent<SpriteRenderer>().sprite = sprites[index];
+
+        GetComponent<SpriteRenderer>().sortingOrder = layer; 
+
+
+
         StartCoroutine(Move());
     }
 
     IEnumerator Move()
     {
+        yield return new WaitForEndOfFrame();
+
+        velocity = _isRight ? velocity * -1 : velocity;
+
         while (true)
         {
             yield return new WaitForFixedUpdate();
 
-            velocity = !_isRight ? velocity * -1: velocity;
 
             transform.Translate(Vector3.right * velocity);
 
@@ -27,4 +41,9 @@ public class CloudBehabiour : MonoBehaviour
 
     }
 
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Spawn" && collision.gameObject.GetComponent<CloudSpawn>()._isRight != _isRight)
+            Destroy(gameObject);
+    }
 }
--- Assets/script/CloudSpawn.cs
+++ Assets/script/CloudSpawn.cs
@@ -7,7 +7,6 @@ public class CloudSpawn : MonoBehaviour
     public Transform _posDown;
     public Transform _posUp;
     public GameObject _cloudRight;
-    public GameObject _cloudLeft;
     public bool _isRight;
 
 
@@ -24,13 +23,13 @@ public class CloudSpawn : MonoBehaviour
         {
             float posY = Random.RandomRange(_posDown.position.y, _posUp.position.y);
 
-            var _cloud = _isRight ? _cloudRight : _cloudLeft;
+            var _cloud = _cloudRight;
 
             var newCloud = Instantiate(_cloud, new Vector3(transform.position.x, posY, transform.position.z), Quaternion.identity);
 
             newCloud.GetComponent<CloudBehabiour>()._isRight = _isRight;
 
-            yield return new WaitForSeconds(1);
+            yield return new WaitForSeconds(0.5f);
         }
 
     }
